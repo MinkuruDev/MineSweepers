c055b5a40c8a78c62802185621048de54121e5ff
Mars <52197560+MinkuruDev@users.noreply.github.com>
2021-07-11 22:49:49 +0700

    Add files via upload

---
+++ MineSweeper/MineSweeper/frmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace MineSweeper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.lblMine = new System.Windows.Forms.Label();
+            this.txtMines = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.txtCols = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Location = new System.Drawing.Point(33, 58);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(119, 41);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "Start";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // lblMine
+            // 
+            this.lblMine.AutoSize = true;
+            this.lblMine.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMine.Location = new System.Drawing.Point(12, 11);
+            this.lblMine.Name = "lblMine";
+            this.lblMine.Size = new System.Drawing.Size(65, 28);
+            this.lblMine.TabIndex = 1;
+            this.lblMine.Text = "Mine: ";
+            this.lblMine.Click += new System.EventHandler(this.lblMine_Click);
+            // 
+            // txtMines
+            // 
+            this.txtMines.Location = new System.Drawing.Point(69, 12);
+            this.txtMines.Name = "txtMines";
+            this.txtMines.Size = new System.Drawing.Size(57, 27);
+            this.txtMines.TabIndex = 2;
+            this.txtMines.Text = "30";
+            this.txtMines.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(156, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rows:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(319, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Columns";
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(224, 12);
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(57, 27);
+            this.txtRows.TabIndex = 5;
+            this.txtRows.Text = "15";
+            this.txtRows.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtCols
+            // 
+            this.txtCols.Location = new System.Drawing.Point(413, 12);
+            this.txtCols.Name = "txtCols";
+            this.txtCols.Size = new System.Drawing.Size(57, 27);
+            this.txtCols.TabIndex = 6;
+            this.txtCols.Text = "15";
+            this.txtCols.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.Location = new System.Drawing.Point(271, 56);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(105, 45);
+            this.lblTime.TabIndex = 7;
+            this.lblTime.Text = "00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 583);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtCols);
+            this.Controls.Add(this.txtRows);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMines);
+            this.Controls.Add(this.lblMine);
+            this.Controls.Add(this.btnStartGame);
+            this.Name = "frmMain";
+            this.Text = "Minesweeper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Label lblMine;
+        private System.Windows.Forms.TextBox txtMines;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.TextBox txtCols;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MineSweeper/MineSweeper/frmMain.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MineSweeper
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            createGround();
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = string.Format("{0:mm\\:ss}", stopWatch.Elapsed);
+        }
+
+        List<Button> buttonList = new List<Button>();
+        int width, height, remain, mineCount,size = 30;
+        int[,] ground;
+        bool[,] flag;
+        bool firstClick = true;
+        Stopwatch stopWatch = new Stopwatch();
+        
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            removeButton();
+            createGround();
+            stopWatch.Reset();
+        }
+
+        private void removeButton()
+        {
+            while (buttonList.Count > 0)
+            {
+                Button temp = buttonList[0];
+                buttonList.Remove(temp);
+                this.Controls.Remove(temp);
+            }
+            firstClick = true;
+        }
+
+        private void onFlag(Button button)
+        {
+            int x = button.Location.X / size;
+            int y = (button.Location.Y - 100) / size;
+            if (flag[x, y])
+            {
+                flag[x, y] = false;
+                button.BackColor = Color.Black;
+            }
+            else
+            {
+                flag[x, y] = true;
+                button.BackColor = Color.Orange;
+            }
+        }
+
+        private void reaval(Button button)
+        {
+            if (button.Enabled == false) return;
+
+            int x = button.Location.X / size;
+            int y = (button.Location.Y - 100) / size;
+            if (flag[x, y]) return;
+
+            if (firstClick)
+            {
+                firstClick = false;
+                stopWatch.Start();
+            }
+
+            button.Enabled = false;
+            remain--;
+            button.BackColor = Color.White;
+
+            if (ground[x,y] == 0)
+            {
+                List<Point> nearbyPoint = getNearbyPoint(x, y);
+                foreach(Point p in nearbyPoint)
+                {
+                    int c = p.X * height + p.Y ;
+                    reaval(buttonList[c]);
+                }
+                return;
+            }
+
+            if(ground[x,y] == -1)
+            {
+                gameOver();
+                return;
+            }
+
+            button.Text = ground[x, y].ToString();
+
+            if (remain == mineCount) gameWin();
+        }
+
+        private void gameWin()
+        {
+            foreach (Button button in buttonList)
+            {
+                button.Enabled = false;
+                stopWatch.Stop();
+                int x = button.Location.X / size;
+                int y = (button.Location.Y - 100) / size;
+                if (ground[x, y] == -1)
+                {
+                    button.BackColor = Color.Orange;
+                }
+            }
+            MessageBox.Show("You Win\nTime: " + lblTime.Text);
+        }
+
+        private void gameOver()
+        {
+            foreach(Button button in buttonList)
+            {
+                button.Enabled = false;
+                stopWatch.Stop();
+                int x = button.Location.X / size;
+                int y = (button.Location.Y - 100) / size;
+                if(ground[x,y] == -1)
+                {
+                    button.Text = "*";
+                    button.BackColor = Color.Orange;
+                }
+            }
+            MessageBox.Show("You Lose");
+        }
+
+        private void createGround()
+        {
+            width = Convert.ToInt32(txtCols.Text);
+            height = Convert.ToInt32(txtRows.Text);
+            mineCount = Convert.ToInt32(txtMines.Text);
+            this.Size = new Size(Math.Max(500, width * 30 + 50), Math.Max(630, height * 30 + 150));
+            remain = width * height;
+
+            ground = new int[width, height];
+            flag = new bool[width, height];
+
+            createMine();
+            createButton();
+            
+        }
+
+        private void createButton()
+        {
+            for (int i = 0; i < width; i -= -1)
+            {
+                for (int j = 0; j < height; j -= -1)
+                {
+                    Button bt = new Button();
+                    this.Controls.Add(bt);
+                    buttonList.Add(bt);
+                    bt.Location = new Point(i * size, j * size + 100);
+                    bt.Size = new Size(size, size);
+                    bt.BackColor = Color.Black;
+                    bt.Enabled = true;
+
+                    bt.MouseDown += (sender, args) =>
+                    {
+                        Button button = (Button)sender;
+                        MouseEventArgs mouseEventArgs = (MouseEventArgs)args;
+                        if (mouseEventArgs.Button == MouseButtons.Right)
+                            onFlag(button);
+                        if (mouseEventArgs.Button == MouseButtons.Left)
+                            reaval(button);
+                    };
+                }
+            }
+        }
+
+        private void createMine()
+        {
+            int i = 0, x, y;
+            while (i < mineCount)
+            {
+                Random random = new Random();
+                x = random.Next(width);
+                y = random.Next(height);
+                if (ground[x, y] == -1) continue;
+
+                addMine(x,y);
+                i -= -1;
+            }
+        }
+
+        private void addMine(int x, int y)
+        {
+            ground[x, y] = -1;
+
+            List<Point> nearbyPoint = getNearbyPoint(x, y);
+            foreach (Point point in nearbyPoint)
+            {
+                if (ground[point.X, point.Y] == -1) continue;
+                ground[point.X, point.Y]++;
+            }
+        }
+
+        List<Point> getNearbyPoint(int x, int y)
+        {
+            List<Point> p = new List<Point>();
+            for(int i = x-1; i<=x+1; i -= -1)
+            {
+                if (i < 0 || i >= width) continue;
+                for(int j = y-1; j<=y+1; j -= -1)
+                {
+                    if (j < 0 || j >= height) continue;
+                    p.Add(new Point(i,j));
+                }
+            }
+            return p;
+        }
+
+        private void lblMine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
